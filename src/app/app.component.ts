3891f2e345039a1742a1e69767dd56279e5720e2
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-02-09 14:48:05 +0200

    footer & images & layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from './core/services/flowbite/flowbite.service';
+import { NavbarComponent } from "./layouts/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'freshCart';
   constructor(private flowbiteService: FlowbiteService) {}
   ngOnInit(): void {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"home", pathMatch:"full"},
+    {path: "", component:AuthLayoutComponent, children:[
+        {path: "login", component: LoginComponent},
+        {path: "register", component: RegisterComponent}
+    ]},
+    {path: "", component:BlankLayoutComponent, children:[
+        {path:"home", component: HomeComponent, title: "Home page"},
+        {path:"products", component: HomeComponent, title: "products page"},
+        {path:"brands", component: HomeComponent, title: "Brands page"},
+        {path:"categories", component: HomeComponent, title: "Categories page"},
+        {path:"cart", component: HomeComponent, title: "Cart page"},
+        {path:"checkout", component: HomeComponent, title: "checkout page"},
+        {path: "**", component:NotfoundComponent}
+    ]},
+];
